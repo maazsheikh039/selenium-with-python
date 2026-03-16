e2df13a8842701f05162553f19053e5332b641b2
maazsheikh039 <maazsheikh039@gmail.com>
2026-03-17 02:04:37 +0500

    Some changes

--- send_email.py
+++ send_email.py
@@ -2,22 +2,23 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
 import os
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+
+
+
 
 driver = webdriver.Firefox()
+wait = WebDriverWait(driver, 10)
 
 # =================================================================================
 
 driver.get("https://workspace.google.com/intl/en-US/gmail/")
 driver.maximize_window()
-driver.implicitly_wait(10)
+# driver.implicitly_wait(10)
 
 # =================================================================================
 
-driver.find_element(By.XPATH, "/html/body/div[1]/gws-header/header/div/div[3]/span[3]/a/span").click()
-# time.sleep(15)
-driver.find_element(By.ID, "identifierId").send_keys("automation20068@gmail.com")
-# time.sleep(5)
-driver.find_element(By.NAME, 'V67aGc').click()
 
 
 
